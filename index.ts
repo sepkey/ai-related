6c046e304deba06270e9f483f43032346362b289
sepKey <sepidekia6990@gmail.com>
2026-07-08 17:10:15 +0330

    feat: add multiple tool calls and create write file

--- index.ts
+++ index.ts
@@ -1,20 +1,22 @@
 import fs from "node:fs";
 import { styleText } from "node:util";
 
-const systemPrompt = `You are a helpful assistant. Please do what user asks. If user ask you about time please respond with {{{get_time}}} if the time is not included in the message otherwise tell the time.
+const systemPrompt = `You are a helpful assistant Please do what user asks.
+User always put update in the query, you should check the updates if the task is already done you should not do it again and continue.
+If user ask you about time please respond with {{{get_time}}} if the time is not included in the message otherwise tell the time.
 
 you have access to the following tools:
 1.get_time {}: returns the current time in a human-readable format.
 2.write_file {fileName:string,content:string}: writes the specified content to a file at the given fileName.
 
 
-you can use tools with the following syntax:
+you can use tools if you need by responding with the following syntax:
 <tool_call>{"name":<name>,"arguments":{...}}</tool_call>
 `;
 
 let context = "";
 async function prompt(query: string) {
-  context += `${context}\n${query}`;
+  context += `${context}\n**UPDATE**\:${query}`;
   console.log(
     [
       styleText(["gray"], "--------------"),
@@ -48,13 +50,13 @@ async function prompt(query: string) {
 
   const content = json.message.content;
   const toolCallMatch = content.match(/<tool_call>(.*?)<\/tool_call>/s);
-  console.log("Tool call match:", toolCallMatch[1]);
-  let toolcall = { name: null, arguments: [] };
-  if (toolCallMatch[1]) {
+
+  let toolcall = { name: null, arguments: {} };
+  if (toolCallMatch?.[1]) {
     try {
       toolcall = JSON.parse(toolCallMatch[1]);
-    } catch (error) {
-      throw new Error(`Invalid tool call format `);
+    } catch {
+      throw Error(`Invalid tool call format `);
     }
   }
 
@@ -82,7 +84,7 @@ async function prompt(query: string) {
 }
 
 const query = process.argv.slice(2).join(" ");
-const { content, toolcall } = await prompt(query);
+let { content, toolcall } = await prompt(query);
 
 function getTime() {
   const now = new Date().toLocaleString();
@@ -104,12 +106,14 @@ while (toolcall.name) {
   switch (toolcall.name) {
     case "get_time": {
       let timeContext = getTime();
-      await prompt(timeContext);
+      const res = await prompt(timeContext);
+      toolcall = res.toolcall;
       break;
     }
     case "write_file": {
       let writeFileContext = writeFile(toolcall.arguments);
-      await prompt(writeFileContext);
+      const res = await prompt(writeFileContext);
+      toolcall = res.toolcall;
       break;
     }
   }
